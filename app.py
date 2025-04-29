41833820880bcdac62c222e81eddbfccee86aeeb
Juan Pablo Ribeiro de Lima <161105749+juanplima@users.noreply.github.com>
2025-04-29 20:53:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+from marshmallow import Schema, fields
 
-# Inicializa o Flask
 app = Flask(__name__)
 
-# Configura o banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://u794777727_BlackBrothers1:senhaTopBD157A$@srv1526.hstgr.io:3306/u794777727_BlackBrotherBD'
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 3600  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
-db = SQLAlchemy()
-ma = Marshmallow()
+class MyModelSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
 
+from models import *  
 
-from models import *
-
-# Agora inicializa db e ma com o app
-db.init_app(app)
-ma.init_app(app)
-
-
-from routes import *
-
-# Criar as tabelas no banco de dados
 with app.app_context():
     db.create_all()
 
-# Roda o app
+from routes import *
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
