fc4f3c339eeb337425d7741be4e22572f745103e
Juan Pablo Ribeiro de Lima <161105749+juanplima@users.noreply.github.com>
2025-05-07 22:36:19 -0300

    Update app.py
    mudei

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow  
+from flask_marshmallow import Marshmallow
 
 # Inicializa o Flask
 app = Flask(__name__)
 
 # Configura o banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://u794777727_BlackBrothers1:senhaTopBD157A$@srv1526.hstgr.io:3306/u794777727_BlackBrotherBD'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://u794777727_BlackBrothers1:senhanovaBD157@193.203.175.99/u794777727_BlackBrotherBD'
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 3600  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "pool_pre_ping": True
+}
 
 
 db = SQLAlchemy()
